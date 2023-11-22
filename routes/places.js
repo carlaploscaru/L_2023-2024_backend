1452be87623750d39b782ca1287cc741e73ed85a
carlaploscaru <101560739+carlaploscaru@users.noreply.github.com>
2023-11-23 00:01:01 +0200

    add property

--- routes/places.js
+++ routes/places.js
@@ -3,8 +3,8 @@ const placeController = require("../controllers/place");
 const isAuth = require("../middleware/is-auth")
 const router = express.Router();
 
-router.post('/add-place', isAuth, placeController.addPlace);//pt folosire middleware, is_auth face verificarea autenticitatii//router.post("/", placeController.addPlace);
-router.get('/get-place', placeController.getPlaces);
+router.post('/', isAuth, placeController.addPlace);//pt folosire middleware, is_auth face verificarea autenticitatii//router.post("/", placeController.addPlace);
+router.get('/', placeController.getPlaces);
 router.patch('/edit-place', isAuth, placeController.editPlace);
 
 
