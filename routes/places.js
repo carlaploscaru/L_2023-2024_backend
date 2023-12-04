876914ad0e5001aa414bd7d05b0a8157ba1a252f
carlaploscaru <101560739+carlaploscaru@users.noreply.github.com>
2023-12-04 15:15:15 +0200

    error handling for new property, edit +delete

--- routes/places.js
+++ routes/places.js
@@ -1,13 +1,84 @@
 const express = require("express");
 const placeController = require("../controllers/place");
-const isAuth = require("../middleware/is-auth")
+const isAuth = require("../middleware/is-auth");
+const { body } = require("express-validator");
+const { Category } = require("../models/category");
 const router = express.Router();
 
-router.post('/', isAuth, placeController.addPlace);//pt folosire middleware, is_auth face verificarea autenticitatii//router.post("/", placeController.addPlace);
+
+router.post('/', [
+    body("title")
+        .trim()
+        .isLength({ min: 3 })
+        .withMessage("Titlul trebuie sa contina minimum 3 caractere"),
+    body("suprafata")
+        .trim()
+        .isNumeric()
+        .withMessage("Suprafata trebuie sa fie numerica"),
+    body("tara")
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage("Tara trebuie sa contina minimum 1 caracter"),
+    body("oras")
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage("Orasul trebuie sa contina minimum 1 caracter"),
+    body("strada")
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage("Strada trebuie sa contina minimum 1 caracter"),
+    body("judet")
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage("Judetul trebuie sa contina minimum 1 caracter"),
+    body("categoryId").custom((value, { req }) => {
+        console.log(value);
+        return Category.findOne({ _id: value }).then((categoryDoc) => {
+            console.log("categoryDoc", categoryDoc);
+            if (!categoryDoc) {
+                return Promise.reject("Aceasta categorie nu exista!");
+            }
+        });
+    }),
+], isAuth, placeController.addPlace);//pt folosire middleware, is_auth face verificarea autenticitatii//router.post("/", placeController.addPlace);
 router.get('/', placeController.getPlaces);
 router.get('/:placeId', placeController.getPlaceById)
-router.patch('/:placeId', isAuth, placeController.editPlace);
+router.patch('/:placeId', [
+    body("title")
+        .trim()
+        .isLength({ min: 3 })
+        .withMessage("Titlul trebuie sa contina minimum 3 caractere"),
+    body("suprafata")
+        .trim()
+        .isNumeric()
+        .withMessage("Suprafata trebuie sa fie numerica"),
+    body("tara")
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage("Tara trebuie sa contina minimum 1 caracter"),
+    body("oras")
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage("Orasul trebuie sa contina minimum 1 caracter"),
+    body("strada")
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage("Strada trebuie sa contina minimum 1 caracter"),
+    body("judet")
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage("Judetul trebuie sa contina minimum 1 caracter"),
+    body("categoryId").custom((value, { req }) => {
+        console.log(value);
+        return Category.findOne({ _id: value }).then((categoryDoc) => {
+            if (!categoryDoc) {
+                return Promise.reject("Aceasta categorie nu exista!");
+            }
+        });
+    }),
+], isAuth, placeController.editPlace);
 
 
+router.delete("/:placeId", isAuth, placeController.deletePlace)
 
 module.exports = router;
